d899451cd34a7bd3aa6d39be44cb3a26859e0e8d
Lux Solari <luxsolari@outlook.com>
2022-06-25 22:45:02 -0300

    Remover SFML de proyecto de evaluacion

--- Evaluacion-2/Evaluacion-2/Program.cs
+++ Evaluacion-2/Evaluacion-2/Program.cs
@@ -1,10 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using SFML.Graphics;
-using SFML.System;
-using SFML.Window;
-
-VideoMode videoMode = VideoMode.DesktopMode;
-RenderWindow renderWindow = new RenderWindow(videoMode, "TEST");
 Console.WriteLine("Hello, World!");
 Console.ReadKey();
