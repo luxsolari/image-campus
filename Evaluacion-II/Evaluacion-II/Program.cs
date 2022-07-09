afe04b168ca436ed8fbe8f4cccf61036a24b14ab
Lux Solari <luxsolari@outlook.com>
2022-07-09 01:23:05 -0300

    commit de archivos faltantes

--- Evaluacion-II/Evaluacion-II/Program.cs
+++ Evaluacion-II/Evaluacion-II/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Evaluacion_II;
+﻿using System.Text.Json;
+using Evaluacion_II.Champions;
+
+namespace Evaluacion_II;
 public static class Program
 {
     public static void Main(string[] args)
     {
-        Game game = new Game();
-        Thread thread1 = new Thread(game.Play);
-        
+        string fileName = "playerChampions.json";
+        string fileText = File.Exists(fileName) ? File.ReadAllText(fileName) : "";
+        List<Champion>? champions = File.Exists(fileName) ? JsonSerializer.Deserialize<List<Champion>>(fileText) : new List<Champion>();
+
+        Champion player1Champion = ChampionGenerator.CreateNewChampion();
+        Champion player2Champion = ChampionGenerator.CreateNewChampion();
+
+        champions?.Add(player1Champion);
+        champions?.Add(player2Champion);
+        string jsonString = JsonSerializer.Serialize(champions);
+        File.WriteAllText(fileName, jsonString);
+
+        fileText = File.ReadAllText(fileName);
+        champions = File.Exists(fileName) ? JsonSerializer.Deserialize<List<Champion>>(fileText) : new List<Champion>();
+        if (champions != null)
+            foreach (Champion champion in champions)
+            {
+                champion.PrintInfo();
+                Console.WriteLine("------------------------------------------------------");
+            }
+
+        Console.ReadKey();
     }
 }
